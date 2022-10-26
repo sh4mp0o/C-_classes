a0117a0056921e9ad43f1e5d5d756f5e437c04a2
shampoo <103971785+sh4mp0o@users.noreply.github.com>
2022-10-26 19:15:05 +0300

    Update lab3_1.cs

--- lab3_1.cs
+++ lab3_1.cs
@@ -28,24 +28,11 @@ namespace lab2_1
             get { return _length; }
             set
             {
-                try
-                {
-                    if (value < 20 || value > 400)
-                        throw new ArgumentException("Недопустимое значение длины судна.");
-                    else
-                        _length = value;
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(System.DateTime.Now.ToString());
-                    Console.WriteLine(ex.Message);
-                    Console.ReadLine();
-                    Environment.Exit(0);
-                }
-                catch(Exception ex)
-                {
-                    Log(ex);
-                }
+                if (value < 20 || value > 400)
+                    throw new ArgumentException("Недопустимое значение длины судна.");
+                else
+                    _length = value;
+
             }
         }
         public string Condition
@@ -53,24 +40,10 @@ namespace lab2_1
             get { return _condition; }
             set
             {
-                try
-                {
-                    if (value != "sunk" && value != "repaired" && value != "broken")
-                        throw new ArgumentException("Недопустимое значение состояния судна.");
-                    else
-                        _condition = value;
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(System.DateTime.Now.ToString());
-                    Console.WriteLine(ex.Message);
-                    Console.ReadLine();
-                    Environment.Exit(0);
-                }
-                catch (Exception ex)
-                {
-                    Log(ex);
-                }
+                if (value != "sunk" && value != "repaired" && value != "broken")
+                    throw new ArgumentException("Недопустимое значение состояния судна.");
+                else
+                    _condition = value;
             }
         }
         public bool IsPassanger
@@ -96,24 +69,10 @@ namespace lab2_1
             get { return _size; }
             set
             {
-                try
-                {
-                    if (value != "small" && value != "medium" && value != "big")
-                        throw new ArgumentException("Недопустимое значение размера судна.");
-                    else
-                        _size = value;
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(System.DateTime.Now.ToString());
-                    Console.WriteLine(ex.Message);
-                    Console.ReadLine();
-                    Environment.Exit(0);
-                }
-                catch (Exception ex)
-                {
-                    Log(ex);
-                }
+                if (value != "small" && value != "medium" && value != "big")
+                    throw new ArgumentException("Недопустимое значение размера судна.");
+                else
+                    _size = value;
             }
         }
         abstract public void Move();
@@ -135,32 +94,18 @@ namespace lab2_1
         }
         virtual public void Repair()
         {
-            try
+            if (this.Condition == "broken")
             {
-                if (this.Condition == "broken")
-                {
-                    this.Condition = "repaired";
-                    Console.WriteLine("Судно успешно отремонтировано.\n" +
-                        "------------------------------------------------------------------------------");
-                }
-                else if (this.Condition == "sunk")
-                {
-                    throw new RepairException("Потонувшие суда нельзя починить!");
-                }
-                else Console.WriteLine("Судно не нуждается в ремонте.\n" +
+                this.Condition = "repaired";
+                Console.WriteLine("Судно успешно отремонтировано.\n" +
                     "------------------------------------------------------------------------------");
             }
-            catch (RepairException ex)
+            else if (this.Condition == "sunk")
             {
-                Console.WriteLine(System.DateTime.Now.ToString());
-                Console.WriteLine(ex.Message);
-                Console.ReadLine();
-                Environment.Exit(0);
+                throw new RepairException("Потонувшие суда нельзя починить!");
             }
-            catch (Exception ex)
-            {
-                Log(ex);
-            }
+            else Console.WriteLine("Судно не нуждается в ремонте.\n" +
+                "------------------------------------------------------------------------------");
         }
         public static bool operator !=(Ship n1, Ship n2)
         {
@@ -189,24 +134,10 @@ namespace lab2_1
 
         public override void Move()
         {
-            try
-            {
-                if (this.Condition == "repaired")
-                    Console.WriteLine($"Лайнер загрузил {this.Tourists} пассажиров и движется к берегам Карибского бассейна.\n" +
-                        $"------------------------------------------------------------------------------");
-                else throw new RepairException("Сначала отремонтируйте лайнер!");
-            }
-            catch (RepairException ex)
-            {
-                Console.WriteLine(System.DateTime.Now.ToString());
-                Console.WriteLine(ex.Message);
-                Console.ReadLine();
-                Environment.Exit(0);
-            }
-            catch (Exception ex)
-            {
-                Log(ex);
-            }
+            if (this.Condition == "repaired")
+                Console.WriteLine($"Лайнер загрузил {this.Tourists} пассажиров и движется к берегам Карибского бассейна.\n" +
+                    $"------------------------------------------------------------------------------");
+            else throw new RepairException("Сначала отремонтируйте лайнер!");
         }
         public Liner() : this("repaired", "medium", 100, 1500) { }
         public Liner(string condition, string size, int length, int touristcnt) : base(condition, size, length)
@@ -220,25 +151,11 @@ namespace lab2_1
             get { return _tourists_count; }
             set
             {
-                try
+                if (value > 0 && value < 5000)
                 {
-                    if (value > 0 && value < 5000)
-                    {
-                        _tourists_count = value;
-                    }
-                    else throw new ArgumentException("Количество туристов не может быть отрицательным и превышать 5000!");
+                    _tourists_count = value;
                 }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(System.DateTime.Now.ToString());
-                    Console.WriteLine(ex.Message);
-                    Console.ReadLine();
-                    Environment.Exit(0);
-                }
-                catch (Exception ex)
-                {
-                    Log(ex);
-                }
+                else throw new ArgumentException("Количество туристов не может быть отрицательным и превышать 5000!");
             }
         }
         public override string ToString()
@@ -255,24 +172,10 @@ namespace lab2_1
         private int _containerscnt;
         public override void Move()
         {
-            try
-            {
-                if (this.Condition == "repaired")
-                    Console.WriteLine($"Грузовой корабль перевозит {this.Containers} контейнеров.\n" +
-                        $"------------------------------------------------------------------------------");
-                else throw new RepairException("Сначала отремонтируйте грузовое судно!");
-            }
-            catch (RepairException ex)
-            {
-                Console.WriteLine(System.DateTime.Now.ToString());
-                Console.WriteLine(ex.Message);
-                Console.ReadLine();
-                Environment.Exit(0);
-            }
-            catch (Exception ex)
-            {
-                Log(ex);
-            }
+            if (this.Condition == "repaired")
+                Console.WriteLine($"Грузовой корабль перевозит {this.Containers} контейнеров.\n" +
+                    $"------------------------------------------------------------------------------");
+            else throw new RepairException("Сначала отремонтируйте грузовое судно!");
         }
         public Cargo_Ship() : this("repaired", "big", 200, 15000) { }
         public Cargo_Ship(string condition, string size, int length, int contscnt) : base(condition, size, length)
@@ -286,22 +189,8 @@ namespace lab2_1
             get { return _containerscnt; }
             set
             {
-                try
-                {
-                    if (value > 0 && value < 18000) { _containerscnt = value; }
-                    else throw new ArgumentException("Количество контейнеров не может быть отрицательным и превышать 18000!");
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(System.DateTime.Now.ToString());
-                    Console.WriteLine(ex.Message);
-                    Console.ReadLine();
-                    Environment.Exit(0);
-                }
-                catch (Exception ex)
-                {
-                    Log(ex);
-                }
+                if (value > 0 && value < 18000) { _containerscnt = value; }
+                else throw new ArgumentException("Количество контейнеров не может быть отрицательным и превышать 18000!");
             }
         }
         public override string ToString()
@@ -322,49 +211,21 @@ namespace lab2_1
             get { return _sailcnt; }
             set
             {
-                try
-                {
-                    if (value > 0 && value < 11) _sailcnt = value;
-                    else throw new ArgumentException("Количество палуб не может быть отрицательным и превышать 11!");
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(System.DateTime.Now.ToString());
-                    Console.WriteLine(ex.Message);
-                    Console.ReadLine();
-                    Environment.Exit(0);
-                }
-                catch (Exception ex)
-                {
-                    Log(ex);
-                }
+                if (value > 0 && value < 11) _sailcnt = value;
+                else throw new ArgumentException("Количество палуб не может быть отрицательным и превышать 11!");
             }
         }
         virtual public void Wind_Adjustment()
         {
-            try
+            if (this.Condition == "repaired")
             {
-                if (this.Condition == "repaired")
-                {
-                    Random rnd = new Random();
-                    if (rnd.Next(1, 3) == 1) Console.WriteLine("Парусник успешно настроился по ветру.\n" +
-                        "------------------------------------------------------------------------------");
-                    else Console.WriteLine("Штиль! Никакого ветра нет.\n" +
-                        "------------------------------------------------------------------------------");
-                }
-                else throw new RepairException("Сначала отремонтируйте судно!");
+                Random rnd = new Random();
+                if (rnd.Next(1, 3) == 1) Console.WriteLine("Парусник успешно настроился по ветру.\n" +
+                    "------------------------------------------------------------------------------");
+                else Console.WriteLine("Штиль! Никакого ветра нет.\n" +
+                    "------------------------------------------------------------------------------");
             }
-            catch (RepairException ex)
-            {
-                Console.WriteLine(System.DateTime.Now.ToString());
-                Console.WriteLine(ex.Message);
-                Console.ReadLine();
-                Environment.Exit(0);
-            }
-            catch (Exception ex)
-            {
-                Log(ex);
-            }
+            else throw new RepairException("Сначала отремонтируйте судно!");
         }
     }
     class Corvette : Sailboat
@@ -378,24 +239,10 @@ namespace lab2_1
         }
         public override void Move()
         {
-            try
-            {
-                if (this.Condition == "repaired")
-                    Console.WriteLine($"{this.Sailboatcnt}-палубный корвет бороздит просторы морей.\n" +
-                        $"------------------------------------------------------------------------------");
-                else throw new RepairException("Сначала отремонтируйте корвет!");
-            }
-            catch (RepairException ex)
-            {
-                Console.WriteLine(System.DateTime.Now.ToString());
-                Console.WriteLine(ex.Message);
-                Console.ReadLine();
-                Environment.Exit(0);
-            }
-            catch (Exception ex)
-            {
-                Log(ex);
-            }
+            if (this.Condition == "repaired")
+                Console.WriteLine($"{this.Sailboatcnt}-палубный корвет бороздит просторы морей.\n" +
+                    $"------------------------------------------------------------------------------");
+            else throw new RepairException("Сначала отремонтируйте корвет!");
         }
         public override string ToString()
         {
@@ -408,6 +255,21 @@ namespace lab2_1
     }
     internal class Program
     {
+        static public void Log(Exception ex)
+        {
+            Console.WriteLine("Что-то пошло не так.");
+            Console.WriteLine(System.DateTime.Now.ToString());
+            Console.WriteLine(ex.Message);
+            using (StreamWriter sw = new StreamWriter("logs.txt", true))
+            {
+                sw.WriteLine(System.DateTime.Now.ToString());
+                sw.WriteLine(ex.Message);
+                sw.WriteLine(ex.StackTrace);
+                sw.WriteLine(ex.InnerException);
+                sw.WriteLine(ex.TargetSite);
+                sw.WriteLine(ex.Data);
+            }
+        }
         static void Main(string[] args)
         {
             Ship[] ships = new Ship[3];
@@ -450,25 +312,32 @@ namespace lab2_1
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("Что-то пошло не так.");
-                    Console.WriteLine(System.DateTime.Now.ToString());
-                    Console.WriteLine(ex.Message);
-                    using (StreamWriter sw = new StreamWriter("logs.txt", true))
-                    {
-                        sw.WriteLine(System.DateTime.Now.ToString());
-                        sw.WriteLine(ex.Message);
-                        sw.WriteLine(ex.StackTrace);
-                        sw.WriteLine(ex.InnerException);
-                        sw.WriteLine(ex.TargetSite);
-                        sw.WriteLine(ex.Data);
-                    }
+                    Log(ex);
                 }
             }
             foreach (var ship in ships)
             {
-                ship.Repair();
-                ship.Move();
-                Console.WriteLine(ship.ToString());
+                try
+                {
+                    ship.Repair();
+                    ship.Move();
+                    Console.WriteLine(ship.ToString());
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(System.DateTime.Now.ToString());
+                    Console.WriteLine(ex.Message);
+
+                }
+                catch (RepairException ex)
+                {
+                    Console.WriteLine(System.DateTime.Now.ToString());
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Log(ex);
+                }
             }
             Console.ReadKey();
         }
